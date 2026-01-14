0490ddcc60941933126b3acca97735125b3e2988
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-14 23:53:37 +0500

    Fourth-Updation

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const Addtask = () =>{
+const Addtask = () => {
+
     let input = document.getElementById('input');
-    let valText = document.createTextNode(input.value);
     let list = document.getElementById('list');
-    var li = document.createElement('li');
-    li.appendChild(valText)
-    list.appendChild(li)
-    input.value = ""; 
-}
 
+    if (input.value.trim() === '') {
+        alert('Please enter a task');
+        return;
+    }
 
+    // Create li
+    let li = document.createElement('li');
+    li.className = 'box';
+
+    // Task text
+    let text = document.createTextNode(input.value);
+    li.appendChild(text);
+
+    // Delete icon
+    let icon = document.createElement('i');
+    icon.className = 'ri-delete-bin-line';
+
+    icon.onclick = () => {
+        li.remove();
+    };
+
+    li.appendChild(icon);
+    list.appendChild(li);
+
+    input.value = '';
+};
