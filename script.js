eb8148d6131387d9f3c90a60331b96d102e0affd
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-14 23:18:42 +0500

    Third-Updation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const Addtask = () =>{
-    
+    let input = document.getElementById('input');
+    let valText = document.createTextNode(input.value);
+    let list = document.getElementById('list');
+    var li = document.createElement('li');
+    li.appendChild(valText)
+    list.appendChild(li)
+    input.value = ""; 
 }
+
+
